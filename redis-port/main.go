13526ca0ea1beedc38c739014a69c3a5b5bddc5a
bingoohuang <bingoo.huang@gmail.com>
2020-11-14 13:43:05 +0800

    refactor

--- redis-port/main.go
+++ redis-port/main.go
@@ -7,37 +7,36 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
-var ctx = context.Background()
-
 func main() {
 	from := redis.NewClient(&redis.Options{
 		Addr:     "192.168.126.5:36794",
-		Password: "xaaU123~", // no password set
-		DB:       0,          // use default DB
+		Password: "xaaU123~",
+		DB:       0, // use default DB
+		PoolSize: 1,
 	})
 	defer from.Close()
 
 	to := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
+		DB:       0, // use default DB
+		PoolSize: 1,
 	})
 	defer to.Close()
 
 	const hkey = `__gateway_redis__`
 
+	ctx := context.Background()
+
 	m, err := from.HGetAll(ctx, hkey).Result()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
 	for k, v := range m {
-		hSet := to.HSet(ctx, hkey, k, v)
-		if hSet.Err() != nil {
-			log.Fatal(err)
+		if hSet := to.HSet(ctx, hkey, k, v); hSet.Err() != nil {
+			panic(err)
 		}
 
 		log.Println(k, " was ported!")
 	}
-
 }
